d44fba754b941607a4eb9d249fd48bef0fc0eebe
lm101845 <1018457683@qq.com>
2022-11-21 22:52:14 +0800

    第12-13集看完

---
+++ 11-尹单人(好)/02-代码手敲/06-path.js
@@ -0,0 +1,70 @@
+/**
+ * @Author liming
+ * @Date 2022/11/21 20:55
+ **/
+
+const path = require('path');
+
+//1.获取路径中的基础名称
+/**
+ * 1.返回的就是接收路径当中的【最后一部分】
+ * 2.第二个参数表示扩展名，如果说没有设置则返回完整的文件名称带后缀
+ * 3.第二个参数作为后缀时，如果没有在当前路径中被匹配到，那么就会忽略
+ * 4.处理目录路径的时候如果说，结尾处有路径分割符，则也会被忽略掉
+ */
+console.log(__filename,'--完整的')   //当前脚本文件执行的完整路径
+//E:\01-code\06-LearnNode★\LearnNode\11-尹单人(好)\02-代码手敲\06-path.js
+
+console.log(path.basename(__filename),'--通过basename处理过的结果')  //path.basename() 方法返回一个 path 的最后一部分
+// 06-path.js
+
+console.log(path.basename(__filename,'.js'),'--如果后缀对的，就不返回后缀了')
+//06-path
+
+console.log(path.basename(__filename,'.css'),'--如果后缀不对，就把后缀整体返回了')
+//06-path.js
+
+
+console.log(path.basename('/a/b/c'))  //返回的就是path路径的【最后一个部分】，不管这个部分是文件还是一个目录
+//c
+
+console.log(path.basename('/a/b/c/'))  //如果结果有目录分隔符/,它会自动忽略掉，就跟没有一样
+//c
+
+
+console.log("====================================")
+
+//2.获取路径目录名(路径)
+/**
+ * 1.返回路径中最后一个部分的上一层目录所在路径
+ * path.dirname() 方法返回一个 path 的目录名
+ */
+console.log(path.dirname(__filename))
+//E:\01-code\06-LearnNode★\LearnNode\11-尹单人(好)\02-代码手敲
+console.log(path.dirname('/a/b/c'))
+///a/b
+console.log(path.dirname('/a/b/c/'))
+///a/b   (仍然没有处理最后面的路径分割符)
+
+console.log("***********************************")
+
+//3.获取路径的扩展名
+/**
+ * 1.返回path路径中相应文件的后缀名
+ * 2.如果path路径中存在多个点，它匹配的是最后一个点，到结尾的内容
+ * path.extname() 方法返回 path 的扩展名，即从 path 的最后一部分中的最后一个 .（句号）字符到字符串结束。
+ * 如果 path 的最后一部分没有 . 或 path 的文件名（见 path.basename()）的第一个字符是 .，则返回一个空字符串。
+ */
+console.log(path.extname(__filename))
+//.js
+console.log(path.extname('/a/b'))
+//空
+console.log(path.extname('/a/b/index.html.js.css'))
+//.css
+
+console.log(path.extname('.index'));
+//空
+
+console.log(path.extname('a.js.'))
+//.
+
---
+++ 11-尹单人(好)/02-代码手敲/06-path1.js
@@ -0,0 +1,171 @@
+/**
+ * @Author liming
+ * @Date 2022/11/21 21:21
+ **/
+const path = require('path')
+//4.解析路径
+/**
+ * 1接收一个路径，返回一个对象，包含不同的信息(root,dir,base,ext,name)
+ */
+const obj = path.parse('/a/b/c/index.html')
+console.log(obj)
+// {
+//     root: '/',           //当前路径的根路径
+//     dir: '/a/b/c',       //最后一个文件的上层目录
+//     base: 'index.html',  //最后一个部分的完整的文件名称(包含文件名 + 后缀)
+//     ext: '.html',        //文件后缀
+//     name: 'index'        //文件名称
+// }
+
+const obj1 = path.parse('/a/b/c')    //绝对路径,root为/
+console.log(obj1)
+//{ root: '/', dir: '/a/b', base: 'c', ext: '', name: 'c' }
+
+const obj2 = path.parse('./a/b/c')   //相对路径，root为空
+console.log(obj2)
+//{ root: '', dir: './a/b', base: 'c', ext: '', name: 'c' }
+
+console.log("=========================")
+
+//5.序列化路径(解析路径(pase)的反操作)
+const obj3 = path.parse('./a/b/c');
+console.log(path.format(obj3))
+//./a/b\c    //这个路径分隔符\和操作系统有关，不用管
+
+//6.判断当前路径是否为绝对路径
+console.log(path.isAbsolute('foo'))
+//false (没有任何分割符修饰，它就认为是非绝对路径)
+
+console.log(path.isAbsolute('/foo'))
+//true
+
+console.log(path.isAbsolute('/////foo'))
+//true  (它会对我们的写法进行一些规范化处理，就算我们写了很多\,它也会认为是一个\)
+
+console.log(path.isAbsolute(''))
+//false
+console.log(path.isAbsolute('.'))
+//false
+console.log(path.isAbsolute('../bar'))
+//false
+
+
+//7.拼接路径
+/**
+ * path.join() 方法使用平台特定的分隔符把全部给定的 path 片段连接到一起，并规范化生成的路径。
+ *
+ * 长度为零的 path 片段会被忽略。 如果连接后的路径字符串是一个长度为零的字符串，则返回 '.'，表示当前工作目录。
+ */
+console.log(path.join('a/b','c','index.html'))
+//a\b\c\index.html     //这个并不是绝对路径的处理
+
+console.log(path.join('/a/b','c','index.html'))
+// \a\b\c\index.html     //这个变成了绝对路径
+
+console.log(path.join('/a/b','c','../','index.html'))
+//\a\b\index.html     它识别到了../(返回上一级),则c就没有了
+
+console.log(path.join('/a/b','c','./','index.html'))
+// \a\b\c\index.html   //./也被识别到了，代表当前同级目录
+
+console.log(path.join('/a/b','c','','index.html'))
+// \a\b\c\index.html      //其中一个路径是空的，会将它忽略掉
+
+console.log(path.join(''))
+//.
+console.log("+++++++++++++++++++++")
+
+//8.规范化路径
+/**
+ * resolve([from],to)
+ * path.normalize() 方法会规范化给定的 path，并解析 '..' 和 '.' 片段。
+ *
+ * 当发现多个连续的路径分隔符时（如 POSIX 上的 / 与 Windows 上的 \ 或 /），它们会被单个的路径分隔符（POSIX 上是 /，Windows 上是 \）替换。
+ * 末尾的多个分隔符会被保留。
+ *
+ * 如果 path 是一个长度为零的字符串，则返回 '.'，表示当前工作目录。
+ */
+console.log(path.normalize('a/b/c/d'))
+//a\b\c\d   //这个路径本身就很规范了，所以就直接打印了
+
+console.log(path.normalize('a///b/c../d'))
+//a\b\c..\d
+
+console.log(path.normalize('a//\\b/c\\/d'))
+// a\b\c\d  这个可以处理
+
+console.log(path.normalize('a//\b/c\\/d'))  //\b本身有其他含义
+//a\c\d
+
+console.log(path.normalize(''))
+//.  返回一个点，代表当前目录
+console.log("--------------------------")
+
+//9.返回绝对路径(我们之前使用的join是拼接路径，如果我们前面不写根路径，那拼接成的基本都是相对路径了)
+/**
+ * path.resolve() 方法会把一个路径或路径片段的序列解析为一个绝对路径。
+ *
+ * 给定的路径的序列是从右往左被处理的，后面每个 path 被依次解析，直到构造完成一个绝对路径。
+ * 例如，给定的路径片段的序列为：/foo、/bar、baz，则调用 path.resolve('/foo', '/bar', 'baz') 会返回 /bar/baz。
+ *
+ * 如果处理完全部给定的 path 片段后还未生成一个绝对路径，则当前工作目录会被用上。
+ *
+ * 生成的路径是规范化后的，且末尾的斜杠会被删除，除非路径被解析为根目录。
+ *
+ * 长度为零的 path 片段会被忽略。
+ *
+ * 如果没有传入 path 片段，则 path.resolve() 会返回当前工作目录的绝对路径。
+ *
+ * https://blog.csdn.net/CarryBest/article/details/88813745
+ */
+console.log(path.resolve())
+//E:\01-code\06-LearnNode★\LearnNode\11-尹单人(好)\02-代码手敲
+//注意和下面2个全局变量进行区分
+console.log(__filename)
+//E:\01-code\06-LearnNode★\LearnNode\11-尹单人(好)\02-代码手敲
+console.log(__dirname)
+// E:\01-code\06-LearnNode★\LearnNode\11-尹单人(好)\02-代码手敲\06-path1.js
+
+console.log("++++++++++++++++++++++++++")
+
+console.log(path.resolve('a','b'));
+//E:\01-code\06-LearnNode★\LearnNode\11-尹单人(好)\02-代码手敲\a\b
+
+console.log(path.resolve('a','/b'));
+//E:\b 有点诡异
+
+console.log(path.resolve('/a','b'));
+// E:\a\b
+
+console.log(path.resolve(__dirname))
+//E:\01-code\06-LearnNode★\LearnNode\11-尹单人(好)\02-代码手敲
+
+console.log(path.resolve(__dirname,'../dist'))
+//E:\01-code\06-LearnNode★\LearnNode\11-尹单人(好)\dist(把上一级目录给去掉了)
+
+console.log(path.resolve(__dirname,'./dist'))
+//E:\01-code\06-LearnNode★\LearnNode\11-尹单人(好)\02-代码手敲\dist
+
+console.log(path.resolve(__dirname,'dist'))
+//E:\01-code\06-LearnNode★\LearnNode\11-尹单人(好)\02-代码手敲\dist
+
+console.log(path.resolve(__dirname,'/dist'))
+//E:\dist  有点诡异
+
+console.log(path.resolve(__dirname,'\dist'))
+//E:\01-code\06-LearnNode★\LearnNode\11-尹单人(好)\02-代码手敲\dist
+
+console.log(path.resolve('/foo/bar', '/tmp/file/'))
+//E:\tmp\file
+
+console.log(path.resolve('/foo/bar', './baz'))
+//E:\foo\bar\baz
+
+console.log(path.resolve('wwwroot', 'static_files/png/', '../gif/image.gif'))
+//E:\01-code\06-LearnNode★\LearnNode\11-尹单人(好)\02-代码手敲\wwwroot\static_files\gif\image.gif
+
+console.log(path.resolve('/foo', '/bar', 'baz'))   //E:\bar\baz
+
+console.log(path.resolve("index.html"))
+//E:\01-code\06-LearnNode★\LearnNode\11-尹单人(好)\02-代码手敲\index.html
+
